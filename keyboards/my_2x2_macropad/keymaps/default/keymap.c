a95a41b6344e7339335f4194f0f6da9bc1661c66
Mugesh-Rajaram <mugeshrajaram@gmail.com>
2025-08-14 06:50:04 +0530

    Add more layers

--- keyboards/my_2x2_macropad/keymaps/default/keymap.c
+++ keyboards/my_2x2_macropad/keymaps/default/keymap.c
@@ -12,7 +12,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * └───┴───┘
      */
     [0] = LAYOUT(
-        KC_3,   KC_4,
+        TO(1),   KC_9,
+        KC_8,   KC_7
+    ),
+    [1] = LAYOUT(
+        TO(2),   KC_6,
+        KC_5,   KC_4
+    ),
+    [2] = LAYOUT(
+        TO(3),   KC_3,
         KC_1,   KC_2
     )
 };
